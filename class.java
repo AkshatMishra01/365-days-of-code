9b983d2dbb5d7ab178af43b0d9d9cbe81a87183e
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2022-01-07 15:00:36 +0530

    Create class.java

---
+++ class.java
@@ -0,0 +1,12 @@
+package jj;
+
+public class Class1 {
+	static int a = 100;
+	static String  str = "Lovely";
+	public static void main(String args[])
+	{
+		Class1 o1 = new Class1();
+		System.out.println(o1.a);
+		System.out.println(o1.str);
+	}
+}
