eec73ffd04e0bd5d43fefb4ab654ade8faf1c35f
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2022-01-20 11:29:09 +0530

    Create Switch.java

---
+++ Switch.java
@@ -0,0 +1,42 @@
+package jj;
+
+public class SwitchString {
+	public static void main(String []args)
+	{
+		int invoke = 4;
+		String monthname = "";
+		switch(invoke) {
+		case 1: monthname = "January";
+		System.out.println(monthname);
+		break;
+		
+		case 2: monthname = "February";
+		System.out.println(monthname);
+		break;
+		
+		case 3: monthname = "March";
+		System.out.println(monthname);
+		break;
+		
+		case 4: monthname = "April";
+		System.out.println(monthname);
+		break;
+		
+		case 5: monthname = "May";
+		System.out.println(monthname);
+		break;
+		
+		case 6: monthname = "June";
+		System.out.println(monthname);
+		break;
+		
+		case 7: monthname = "July";
+		System.out.println(monthname);
+		break;
+		
+		default: System.out.println("Where do you live man ??");
+		
+		}
+	}
+}
+  
