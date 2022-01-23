e49fa534ce5c9cccc04d1cc5002792817035c7b9
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2022-01-23 15:47:14 +0530

    Array

--- Arrays.java
+++ Arrays.java
@@ -1,1 +1,17 @@
+package Arrays;
 
+// pros:
+//Code optimization
+//Random access
+
+//cons: Fixed size at runtime (collection framework for increasing the size at runtime)
+public class a1 {
+	public static void main(String args[])
+	{
+		int a[] = {0,1,2,3,4,5,6,7,8,9};
+		for(int i:a)
+		{
+		System.out.println(a[i]);
+		}
+	}
+}
