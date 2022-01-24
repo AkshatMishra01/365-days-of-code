a8e854c449a37e6883f483a6759e9c42b77d3b91
Akshat Kumar Mishra <59910316+AkshatMishra01@users.noreply.github.com>
2022-01-24 16:37:45 +0530

    Method overloading through argument numbers

---
+++ overloading.java
@@ -0,0 +1,19 @@
+package jj;
+
+class gyro{
+	static int add(int a,int b) {
+		return a+b;
+	}
+	static int add(int a,int b,int e){
+		return a+b+e;
+	}
+}
+
+// number of arguments
+public class overloading {
+
+	public static void main(String args[])
+	{
+		System.out.println(gyro.add(4, 2, 3));
+	}
+}
